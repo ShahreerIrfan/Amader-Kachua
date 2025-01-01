3397caa8b010e627ab94f7b29ad85ab140bd953c
md shahreer irfan <mdshahreerirfan@gmail.com>
2025-01-01 19:44:06 +0600

    User profile api make done

--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-from .models import UserProfile
 from django.contrib.auth.models import User
+from .models import UserProfile
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,8 +8,40 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'first_name', 'last_name']
 
 class UserProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer()  # Nested User serializer
+    user = UserSerializer()
+    password1 = serializers.CharField(write_only=True, required=True)
+    password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = UserProfile
-        fields = '__all__'
+        fields = '__all__'  # Including all fields from UserProfile model and password fields
+
+    def validate(self, data):
+        """
+        Ensure that password1 and password2 are the same.
+        """
+        password1 = data.get('password1')
+        password2 = data.get('password2')
+
+        if password1 != password2:
+            raise serializers.ValidationError("Passwords must match.")
+        return data
+
+    def create(self, validated_data):
+        """
+        Create a new user and user profile.
+        """
+        # Remove password1 and password2 from validated_data before passing to UserProfile
+        password = validated_data.pop('password1')
+
+        # Create the User instance
+        user_data = validated_data.pop('user')  # Remove user data for creating user
+        user = User(**user_data)  # Create the user instance
+        user.set_password(password)  # Hash the password before saving
+        user.save()  # Save the user instance
+
+        # Now create the UserProfile instance with remaining validated data
+        validated_data.pop('password2', None)  # Ensure password2 is not passed to UserProfile
+        user_profile = UserProfile.objects.create(user=user, **validated_data)
+
+        return user_profile
