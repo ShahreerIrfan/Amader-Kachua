38fe8bfcbab0a46b121a3fcd1fd4f2f287e32322
md shahreer irfan <mdshahreerirfan@gmail.com>
2025-01-02 00:07:21 +0600

    Unnecessary code deleted

--- amader_kachua/settings.py
+++ amader_kachua/settings.py
@@ -68,13 +68,23 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     ),
+#     # 'DEFAULT_PERMISSION_CLASSES': (
+#     #     'rest_framework.permissions.IsAuthenticated',
+#     # ),
+# }
+
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    # 'DEFAULT_RENDERER_CLASSES': [
+    #     # 'rest_framework.renderers.JSONRenderer',  # Only JSON output
+    # ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication', 
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-    # 'DEFAULT_PERMISSION_CLASSES': (
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ),
+    ],
 }
 
 
--- user/serializers.py
+++ user/serializers.py
@@ -14,12 +14,9 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserProfile
-        fields = '__all__'  # Including all fields from UserProfile model and password fields
+        fields = '__all__'  
 
     def validate(self, data):
-        """
-        Ensure that password1 and password2 are the same.
-        """
         password1 = data.get('password1')
         password2 = data.get('password2')
 
@@ -28,20 +25,17 @@ class UserProfileSerializer(serializers.ModelSerializer):
         return data
 
     def create(self, validated_data):
-        """
-        Create a new user and user profile.
-        """
-        # Remove password1 and password2 from validated_data before passing to UserProfile
+        
+
         password = validated_data.pop('password1')
 
-        # Create the User instance
-        user_data = validated_data.pop('user')  # Remove user data for creating user
-        user = User(**user_data)  # Create the user instance
-        user.set_password(password)  # Hash the password before saving
-        user.save()  # Save the user instance
 
-        # Now create the UserProfile instance with remaining validated data
-        validated_data.pop('password2', None)  # Ensure password2 is not passed to UserProfile
+        user_data = validated_data.pop('user')  
+        user = User(**user_data)  
+        user.set_password(password)  
+        user.save()  
+
+        validated_data.pop('password2', None)  
         user_profile = UserProfile.objects.create(user=user, **validated_data)
 
         return user_profile
