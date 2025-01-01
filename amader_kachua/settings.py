e2482b582344fba21d846a93dde3af6d73e44b42
md shahreer irfan <mdshahreerirfan@gmail.com>
2025-01-01 22:00:51 +0600

    Cors True

--- amader_kachua/settings.py
+++ amader_kachua/settings.py
@@ -57,7 +57,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
